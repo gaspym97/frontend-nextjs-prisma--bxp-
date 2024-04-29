3905a9b55b0f99dd01ad8d887b7b14ca31463082
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-29 18:50:24 -0400

    working on the noteform component

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,15 +1,42 @@
+"use client"
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
 function NoteForm() {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const router = useRouter()
+
     return (
-        <form>
+        <form
+            onSubmit={async (e) => {
+                e.preventDefault()
+
+                const res = await fetch('/api/notes', {
+                    method: 'POST',
+                    body: JSON.stringify({ title, content }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                const data = await res.json()
+
+                router.refresh()
+            }}
+        >
             <input
                 type="text"
                 name="title" autoFocus
-                placeholder="Title" className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2" />
+                placeholder="Title" className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+                onChange={(e) => setTitle(e.target.value)}
+            />
 
             <textarea
                 name="title"
                 placeholder="Content"
                 className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+                onChange={(e) => setContent(e.target.value)}
             ></textarea>
 
             <button className="px-5 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700">
