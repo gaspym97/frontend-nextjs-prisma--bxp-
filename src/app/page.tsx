2611776b7a317539a54c34014762c0ef2db79fac
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-29 15:34:28 -0400

    coding homepage bases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
-function HomePage() {
+async function loadNotes() {
+  const res = await fetch("http://localhost:3000/api/notes")
+  const data = await res.json()
+  return data
+}
+async function HomePage() {
+  const notes = await loadNotes()
+
   return (
-    <div>HomePage</div>
+    <>
+      <div>
+        {
+          notes.map((note) => (
+            <div key={note.id}>
+              <h1>{note.title}</h1>
+              <p>{note.content}</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
