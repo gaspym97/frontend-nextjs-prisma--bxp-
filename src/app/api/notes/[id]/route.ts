15f8f545374de9e78c848553d1eb15110d68bfc3
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-29 13:42:46 -0400

    building the param apis

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -1,15 +1,59 @@
 import { NextResponse } from 'next/server'
 
-export function GET() {
-    return NextResponse.json({
-        message: "getting individual note"
-    })
+interface Params {
+    params: { id: string }
 }
 
-export function DELETE() {
-    return NextResponse.json({
-        message: "deleting specific note"
-    })
+// gets an individual note
+export async function GET(request: Request, { params }: Params) {
+
+    try {
+        // findFirst obtain a single note
+        const note = await prisma?.note.findFirst({
+            where: {
+                id: Number(params.id)
+            }
+        })
+        // if note not found then
+        if (!note) {
+            return NextResponse.json(
+                { message: "note not found" },
+                { status: 404 }
+            )
+        }
+
+        return NextResponse.json(note)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { message: error.message },
+                { status: 500 }
+            )
+        }
+    }
+}
+
+export async function DELETE(request: Request, { params }: Params) {
+    try {
+        const note = await prisma?.note.delete({
+            where: {
+                id: Number(params.id)
+            }
+        })
+        if (!note) {
+            return NextResponse.json(
+                { message: "note not found" },
+                { status: 404 }
+            )
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { message: error.message },
+                { status: 500 }
+            )
+        }
+    }
 }
 
 export function PUT() {
--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -2,22 +2,39 @@ import { NextResponse } from 'next/server'
 import { prisma } from '@/libs/prisma'
 
 export async function GET() {
-
-    const notes = await prisma.note.findMany()
-    return NextResponse.json(notes)
+    try {
+        const notes = await prisma.note.findMany()
+        return NextResponse.json(notes)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { message: error.message },
+                { status: 500 }
+            )
+        }
+    }
 }
 
 export async function POST(request: Request) {
 
-    // store the data in the expected properties
-    const { title, content } = await request.json()
+    try {
+        // store the data in the expected properties
+        const { title, content } = await request.json()
 
-    // create new note
-    const newNote = await prisma.note.create({
-        data: {
-            title,
-            content
+        // create new note
+        const newNote = await prisma.note.create({
+            data: {
+                title,
+                content
+            }
+        })
+        return NextResponse.json(newNote)
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { message: error.message },
+                { status: 500 }
+            )
         }
-    })
-    return NextResponse.json(newNote)
+    }
 }
