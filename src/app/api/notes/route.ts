75af103cbcea899c03602999525cedcaa74b0af8
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-29 13:02:52 -0400

    coding prisma queries

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,13 +1,23 @@
 import { NextResponse } from 'next/server'
+import { prisma } from '@/libs/prisma'
 
-export function GET() {
-    return NextResponse.json({
-        message: "getting notes from api notes"
-    })
+export async function GET() {
+
+    const notes = await prisma.note.findMany()
+    return NextResponse.json(notes)
 }
 
-export function POST() {
-    return NextResponse.json({
-        message: "creating notes"
+export async function POST(request: Request) {
+
+    // store the data in the expected properties
+    const { title, content } = await request.json()
+
+    // create new note
+    const newNote = await prisma.note.create({
+        data: {
+            title,
+            content
+        }
     })
+    return NextResponse.json(newNote)
 }
