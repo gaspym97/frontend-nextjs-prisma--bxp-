e1b3ac00d6fa93bb445411b098918684ebb83590
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-30 12:11:33 -0400

    delete functionality completed, next up: edit note

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,10 +1,11 @@
 "use client"
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useNotes } from '@/context/NoteContext'
 
 function NoteForm() {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+    const titleRef = useRef<HTMLInputElement>(null)
 
     // import the context
     const { createNote } = useNotes()
@@ -17,6 +18,10 @@ function NoteForm() {
                     title,
                     content
                 })
+                setTitle("")
+                setContent("")
+
+                titleRef.current?.focus()
             }}
         >
             <input
@@ -24,12 +29,15 @@ function NoteForm() {
                 name="title" autoFocus
                 placeholder="Title" className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
                 onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                ref={titleRef}
             />
             <textarea
                 name="title"
                 placeholder="Content"
                 className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
                 onChange={(e) => setContent(e.target.value)}
+                value={content}
             ></textarea>
             <button className="px-5 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700">
                 Create note
