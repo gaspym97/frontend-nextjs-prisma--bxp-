af822a9cc468de710025b32abfea03cef5f181fd
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-30 16:19:05 -0400

    new type added

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,3 +1,6 @@
 import { Note } from '@prisma/client'
 
 export type CreateNote = Omit<Note, 'id' | 'createdAt' | 'updatedAt'>
+
+// all data types will be optional
+export type UpdateNote = Partial<CreateNote>
