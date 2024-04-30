4755a5875e439a9bcce5ce70043b1849134625e9
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-30 11:23:36 -0400

    deleteNote work ok

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,17 @@
+import { GET } from '@/app/api/notes/route'
 import { Note } from '@/interfaces/interfaces'
 
 function NoteCard({ note }: { note: Note }) {
+
+    // function to delete each note
+    async function deleteNote(id: string) {
+        const res = await fetch(`http://localhost:3000/api/notes/${id}`, {
+            method: 'DELETE',
+        })
+        const data = await res.json()
+        console.log(data)
+    }
+
     return (
         <div key={note.id} className='bg-slate-400 p-4 my-2 flex justify-between'>
             <div>
@@ -8,13 +19,7 @@ function NoteCard({ note }: { note: Note }) {
                 <p>{note.content}</p>
             </div>
             <div className='flex gap-x-2'>
-                <button
-                    onClick={() => {
-                        console.log('clicked')
-                    }}
-                >
-                    Delete
-                </button>
+                <button onClick={() => deleteNote(note.id)}>Delete</button>
                 <button>Edit</button>
             </div>
         </div>
